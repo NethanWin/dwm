43a238e8fe6daf8168690a10b5b8b37451e6af5f
nethan <nethan.win@protonmail.com>
2024-08-07 00:11:25 +0300

    working test

---
+++ install.sh
@@ -0,0 +1,41 @@
+# dwm
+# https://forums.bunsenlabs.org/viewtopic.php?id=7571
+# https://www.reddit.com/r/suckless/comments/jj61py/how_do_i_make_dwm_appear_on_my_display_manager/
+sudo pacman -Syyu
+sudo pacman -S --noconfirm --needed base-devel git libx11 libxft xorg-server xorg-xinit terminus-font
+sudo mkdir -p ~/.local/src
+
+sudo cp -r st ~/.local/src/
+sudo cp -r dwm ~/.local/src/
+sudo cp -r dmenu ~/.local/src/
+sudo cp -r dwmblocks ~/.local/src/
+
+# st
+cd ~/.local/src/st
+sudo make clean
+sudo make install
+
+# dmenu
+cd ~/.local/src/dmenu
+sudo sed -i 's/^XINERAMALIBS/# XINERAMALIBS  = -lXinerama/' config.mk
+sudo sed -i 's/^XINERAMAFLAGS/# XINERAMAFLAGS = -DXINERAMA/' config.mk
+sudo make clean
+sudo make install
+
+# dwm
+cd ~/.local/src/dwm
+sudo sed -i 's/^XINERAMALIBS/# XINERAMALIBS  = -lXinerama/' config.mk
+sudo sed -i 's/^XINERAMAFLAGS/# XINERAMAFLAGS = -DXINERAMA/' config.mk
+sudo make clean
+sudo make install
+
+sudo mkdir -p /usr/share/xsessions
+sudo touch /usr/share/xsessions/dwm.desktop
+
+sudo sh -c 'echo "[Desktop Entry]
+Encoding=UTF-8
+Name=dwm
+Comment=Dynamic window manager
+Exec=dwm
+Icon=dwm
+Type=XSession" > /usr/share/xsessions/dwm.desktop'
